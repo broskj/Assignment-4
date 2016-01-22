bcc571636178862d13b7d4a6ecf2fd27dc2ef95c
gdscheele <gdscheele@gmail.com>
2016-01-21 22:29:00 -0500

    working static files. Added some queries to listings.server.controller.js <untested>. got default route working and listingId route working.

--- server/config/express.js
+++ server/config/express.js
@@ -1,37 +1,58 @@
-var path = require('path'),  
-    express = require('express'), 
+var path = require('path'),
+    express = require('express'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     config = require('./config'),
-    listingsRouter = require('../routes/listings.server.routes'), 
+    listingsRouter = require('../routes/listings.server.routes'),
     getCoordinates = require('../controllers/coordinates.server.controller.js');
 
-module.exports.init = function() {
-  //connect to database
-  mongoose.connect(config.db.uri);
+module.exports.init = function () {
+    //connect to database
+    mongoose.connect(config.db.uri);
 
-  //initialize app
-  var app = express();
+    //initialize app
+    var app = express();
 
-  //enable request logging for development debugging
-  app.use(morgan('dev'));
-
-  //body parsing middleware 
-  app.use(bodyParser.json());
-
-  /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
-  app.post('/api/coordinates', getCoordinates, function(req, res) {
-    res.send(req.results);
-  });
-
-  /* serve static files */
-  
-
-  /* use the listings router for requests to the api */
+    //tell app to use jade
+    app.set('views', './client/views')
+    app.set('view engine', 'jade');
 
 
-  /* go to homepage for all routes not specified */ 
+    //enable request logging for development debugging
+    app.use(morgan('dev'));
 
-  return app;
+    //body parsing middleware
+    app.use(bodyParser.json());
+
+    /* server wrapper around Google Maps API to get latitude + longitude coordinates from address */
+    app.post('/api/coordinates', getCoordinates, function (req, res) {
+        res.send(req.results);
+    });
+
+    /* serve static files */
+    //access static files with '/static/styles/main.css' etc
+    app.use('/static', express.static('client'));
+
+    /* use the listings router for requests to the api */
+    app.get('/api/*', function (req, res, next) {
+        console.log('the response will be sent by the next function ...');
+        next();
+    }, function (req, res) {
+        console.log(req.params);
+        listingsRouter.get('listings/' + req.params._id);
+    });
+
+
+    app.get('/', function (req, res, next) {
+        console.log('the response will be sent by the next function ...');
+        next();
+    }, function (req, res) {
+        res.sendFile(path.join(__dirname, '../../client/views', 'index.html'));
+    });
+
+    /* go to homepage for all routes not specified */
+
+
+    return app;
 };  
